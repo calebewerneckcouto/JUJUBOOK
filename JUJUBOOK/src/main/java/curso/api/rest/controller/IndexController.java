162b9ada662bfdb05fc97f8b974ac47d60633b71
Calebe <calebewerneck@hotmail.com>
2024-03-25 16:45:20 -0300

    Adaptado para AWS

--- JUJUBOOK/src/main/java/curso/api/rest/controller/IndexController.java
+++ JUJUBOOK/src/main/java/curso/api/rest/controller/IndexController.java
@@ -24,6 +24,10 @@ public class IndexController {
     	return "index";
     }
     
+    
+   
+    
+    
 
    
 }
--- JUJUBOOK/src/main/java/curso/springboot/security/WebConfigSecurity.java
+++ JUJUBOOK/src/main/java/curso/springboot/security/WebConfigSecurity.java
@@ -1,50 +1,21 @@
 package curso.springboot.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private ImplementacaoUserDetailsService implementacaoUserDetailsService;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/", "/index", "/resenha", "/resenha.html", "/biografia").permitAll()
-                .antMatchers(HttpMethod.GET, "/homecontrole").hasAnyRole("ADMIN", "USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().permitAll()
-                .loginPage("/login")
-                .defaultSuccessUrl("/homecontrole")
-                .failureUrl("/login?error=true")
-                .and()
-                .logout()
-                .logoutSuccessUrl("/login")
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(implementacaoUserDetailsService)
-                .passwordEncoder(new BCryptPasswordEncoder());
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        // Permitir acesso público a recursos estáticos (CSS, JavaScript, imagens, etc.)
-        web.ignoring().antMatchers("/static/**");
+            .authorizeRequests()
+            .antMatchers("/**").permitAll() // Allow access to all endpoints
+            .and()
+            .httpBasic(); // Use HTTP Basic authentication
     }
 }
